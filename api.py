cee5ddd0f8a453df216cf1dcb84069b39ce936bc
NejcCeh <126692291+NejcCeh@users.noreply.github.com>
2025-03-02 19:03:56 +0100

    Add/modify files for the csv-data branch

---
+++ api.py
@@ -0,0 +1,62 @@
+import os
+from bottle import Bottle, run, template, response
+import json
+from modeli import VideoIgra, Zanr, Izdajatelj, Konzola, Ocena
+
+app = Bottle()
+
+
+app.config['template_lookup'] = [os.path.join(os.path.dirname(__file__), "views")]
+
+# Glavna stran s povezavami do kategorij
+@app.route('/')
+def home():
+    return template('index.tpl')
+
+# Strani za prikaz seznamov vseh kategorij
+@app.route('/zanri')
+def seznam_zanrov():
+    zanri = Zanr.vsi_zanri()
+    return template('categories.tpl', tip="žanri", elementi=zanri)
+
+@app.route('/izdajatelji')
+def seznam_izdajateljev():
+    izdajatelji = Izdajatelj.vsi_izdajatelji()
+    return template('categories.tpl', tip="izdajatelji", elementi=izdajatelji)
+
+@app.route('/konzole')
+def seznam_konzol():
+    konzole = Konzola.vse_konzole()
+    return template('categories.tpl', tip="konzole", elementi=konzole)
+
+# Strani za prikaz iger glede na izbrano kategorijo
+@app.route('/igre/zanr/<zanr>')
+def igre_po_zanru(zanr):
+    igre = VideoIgra.igre_po_zanru(zanr)
+    return template('games.tpl', kategorija=zanr, igre=igre)
+
+@app.route('/igre/izdajatelj/<izdajatelj>')
+def igre_po_izdajatelju(izdajatelj):
+    igre = VideoIgra.igre_po_izdajatelju(izdajatelj)
+    return template('games.tpl', kategorija=izdajatelj, igre=igre)
+
+@app.route('/igre/konzola/<konzola>')
+def igre_po_konzoli(konzola):
+    igre = VideoIgra.igre_po_konzoli(konzola)
+    return template('games.tpl', kategorija=konzola, igre=igre)
+
+# vse igre
+@app.route('/igre')
+def vse_igre():
+    igre = VideoIgra.vse_igre()
+    return template('games.tpl', kategorija="Vse igre", igre=igre)
+
+#najbolje ocenjene igre
+@app.route('/igre/najbolje-ocenejene')
+def najbolje_ocenjene_igre():
+    igre = VideoIgra.najbolje_ocenjene_igre()
+    return template('games.tpl', kategorija="Najbolje ocenjene igre", igre=igre)
+
+# Zagon strežnika
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080, debug=True)
--- import_csv.py
+++ import_csv.py
@@ -17,12 +17,12 @@ def import_csv(filename, table):
     conn.commit()
     conn.close()
 
-# Uvoz vseh podatkov iz CSV datotek
-# import_csv("video_igre.csv", "video_igre")
-# import_csv("izdajatelji.csv", "izdajatelji")
-# import_csv("igre_izdajatelj.csv", "igre_izdajatelj")
-# import_csv("konzole.csv", "konzole")
-# import_csv("igre_konzole.csv", "igre_konzole")
-# import_csv("zanri.csv", "zanri")
-# import_csv("igre_zanri.csv", "igre_zanri")
-# import_csv("ocene.csv", "ocene")
+#Uvoz vseh podatkov iz CSV datotek
+import_csv("video_igre.csv", "video_igre")
+import_csv("izdajatelji.csv", "izdajatelji")
+import_csv("igre_izdajatelj.csv", "igre_izdajatelj")
+import_csv("konzole.csv", "konzole")
+import_csv("igre_konzole.csv", "igre_konzole")
+import_csv("zanri.csv", "zanri")
+import_csv("igre_zanri.csv", "igre_zanri")
+import_csv("ocene.csv", "ocene")
